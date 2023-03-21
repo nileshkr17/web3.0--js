add355ac3baed088e30332d4324c7e3fe0af2fa0
Nilesh <ece.20becd76@silicon.ac.in>
2023-03-21 19:07:10 +0530

    [thunder-cloud-api-check]

--- transaction/app.js
+++ transaction/app.js
@@ -1,24 +1,44 @@
-// const express = require('express')
-// const app = express()
-// const PORT = 1234
-// app.listen(PORT,()=>{
-//     console.log('Server is running on',PORT)
-// })
-
+const express = require('express')
 const Block = require('./block')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
 
-let transaction = new Transaction ('Nilesh','Singh',100)
+const app = express()
+app.use(express.json())
+const PORT = 1234
 
-let genesisBlock = new Block()
-let blockchain = new Blockchain(genesisBlock)
-let block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
+//an array to store all the transactions -nilesh
+let transaction= []
 
-let check_tran = new Transaction('Nilesh','Suman',500)
-let block1 = blockchain.getNextBlock([check_tran])
-blockchain.addBlock(block1)
+app.post('/transactions',(req,res)=>{
+    const to =req.body.to
+    const from = req.body.from
+    const amount = req.body.amount
+
+    //create a new transaction -nilesh
+    let new_transaction = new Transaction(from,to,amount)
+    transaction.push(new_transaction)
+    res.json(transaction)
+})
 
 
-console.log(blockchain)
+
+app.get('/blockchain',(req,res)=>{
+    
+    let transaction = new Transaction ('Nilesh','Saheel',100)
+    let genesisBlock = new Block()
+    let blockchain = new Blockchain(genesisBlock)
+    let block = blockchain.getNextBlock([transaction])
+    blockchain.addBlock(block)
+    let check_tran = new Transaction('Nilesh','Suman',500)
+    let block1 = blockchain.getNextBlock([check_tran])
+    blockchain.addBlock(block1)
+    res.json(blockchain)
+
+})
+
+
+app.listen(PORT,()=>{
+    console.log('Server is running on',PORT)
+})
+
