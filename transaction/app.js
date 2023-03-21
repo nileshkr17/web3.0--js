e571bc772dadeae36fd391710726052e458cef03
Nilesh <ece.20becd76@silicon.ac.in>
2023-03-21 18:39:01 +0530

    [working-fine-local]

--- transaction/app.js
+++ transaction/app.js
@@ -1,3 +1,10 @@
+// const express = require('express')
+// const app = express()
+// const PORT = 1234
+// app.listen(PORT,()=>{
+//     console.log('Server is running on',PORT)
+// })
+
 const Block = require('./block')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
