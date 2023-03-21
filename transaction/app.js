669966dca56c4246d86fd187983e42062e04a65e
Nilesh <ece.20becd76@silicon.ac.in>
2023-03-21 19:17:08 +0530

    [multiple-tranaction-in-single-block]

--- transaction/app.js
+++ transaction/app.js
@@ -7,9 +7,12 @@ const app = express()
 app.use(express.json())
 const PORT = 1234
 
+let genesisBlock = new Block()
+let blockchain = new Blockchain(genesisBlock)
+
 //an array to store all the transactions -nilesh
 let transaction= []
-
+//create a new transaction -nilesh
 app.post('/transactions',(req,res)=>{
     const to =req.body.to
     const from = req.body.from
@@ -21,19 +24,24 @@ app.post('/transactions',(req,res)=>{
     res.json(transaction)
 })
 
-
+app.get('/mine',(req,res)=>{
+    let block = blockchain.getNextBlock(transaction)
+    blockchain.addBlock(block)
+    res.json(block)
+})
 
 app.get('/blockchain',(req,res)=>{
     
-    let transaction = new Transaction ('Nilesh','Saheel',100)
-    let genesisBlock = new Block()
-    let blockchain = new Blockchain(genesisBlock)
-    let block = blockchain.getNextBlock([transaction])
-    blockchain.addBlock(block)
-    let check_tran = new Transaction('Nilesh','Suman',500)
-    let block1 = blockchain.getNextBlock([check_tran])
-    blockchain.addBlock(block1)
     res.json(blockchain)
+    // let transaction = new Transaction ('Nilesh','Saheel',100)
+    // let genesisBlock = new Block()
+    // let blockchain = new Blockchain(genesisBlock)
+    // let block = blockchain.getNextBlock([transaction])
+    // blockchain.addBlock(block)
+    // let check_tran = new Transaction('Nilesh','Suman',500)
+    // let block1 = blockchain.getNextBlock([check_tran])
+    // blockchain.addBlock(block1)
+    // res.json(blockchain)
 
 })
 
